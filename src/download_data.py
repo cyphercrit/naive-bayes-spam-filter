ad746628a00d28a7dcc915c0535f4bd261d7c448
Caden Jurkovski <caden.jurkovski@outlook.com>
2026-04-26 15:35:46 -0500

    Finished data downloader

--- src/download_data.py
+++ src/download_data.py
@@ -21,25 +21,36 @@ class DownloadData:
         self.spam_directory = Path("./data/spam/")
         self.spam_directory.mkdir(parents=True, exist_ok=True)
 
-    def download_data(self, file_name: str, base_url: str) -> None:
-        save_path = self.raw_directory + file_name
-        if Path(save_path).exists():
+    def download_file(self, file_name: str) -> None:
+        save_path = self.raw_directory / file_name
+        if save_path.exists():
             return
         
         try:
-            with requests.get(base_url+file_name) as r:
+            with requests.get(self.base_url + file_name, stream=True) as r:
                 r.raise_for_status()
                 with open(save_path, 'wb') as f:
                     for chunk in r.iter_content(chunk_size=8192):
                         f.write(chunk)
         except Exception as e:
-            print(f"Encountered exception when trying to download file: {file_name} from {base_url}")
+            print(f"Encountered exception when trying to download file: {file_name} from {self.base_url}")
             raise e
 
     def extract_data(self, file_name: str) -> None:
-        pass
+        from_path = self.raw_directory / file_name
+        to_path = (self.ham_directory / file_name if "ham" in file_name else self.spam_directory / file_name)
 
-    def download_data(self):
+        try:
+            with tarfile.open(from_path, "r:bz2") as tar:
+                tar.extractall(path=to_path)
+        except Exception as e:
+            print(f"Encountered exception when trying to extract file: {file_name}")
+
+    def download_data(self) -> None:
         for file_name in self.file_names:
-            self.download_data(file_name)
+            self.download_file(file_name)
             self.extract_data(file_name)
+    
+if __name__ == "__main__":
+    data_downloader = DownloadData(["20021010_easy_ham.tar.bz2", "20030228_spam.tar.bz2"], "https://spamassassin.apache.org/old/publiccorpus/")
+    data_downloader.download_data()
